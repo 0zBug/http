801a47653abdea09ac90967d70a17d065112cbdd
Bug <77031032+0zBug@users.noreply.github.com>
2023-04-16 20:00:31 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+local request = syn and syn.request or request
+
+return setmetatable({}, {
+    __index = function(self, Method)
+        return function(Url, Args)
+            local Data = {}
+            Data.Url = Url
+            Data.Method = Method
+
+            for Index, Value in next, Args or {} do
+                Data[string.upper(string.sub(Index, 1, 1)) .. string.lower(string.sub(Index, 2, -1))] = Value
+            end
+
+            return setmetatable(request(Data), {
+                __tostring = function(self)
+                    return self.Body
+                end
+            })
+        end
+    end
+})
